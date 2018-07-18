2c13ec08cd01221dccfe1e666c20f6eda1b1aeaa
Jose Albizures <albizures3601@gmail.com>
2018-07-17 21:11:56 -0600

    add react-native-localization and create Translation component

---
+++ src/components/Translate.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import { Text } from 'react-native'
+import LocalizedStrings from 'react-native-localization';
+
+const strings = new LocalizedStrings({
+  en: require('../translations/en.json'),
+  es: require('../translations/es.json'),
+})
+
+const Translate = ({keyName}) => (
+  <Text>{strings[keyName]}</Text>
+)
+
+Translate.propTypes = {
+  keyName: PropTypes.string.isRequired
+}
+
+export default Translate
