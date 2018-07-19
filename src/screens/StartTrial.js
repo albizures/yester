96c97a4f9b9fa3c5714e0cf2a32eda940b98d9c2
gluix20 <lx.galvez@gmail.com>
2018-07-19 15:06:13 -0600

    Change translation keyNames to new standard

--- src/screens/StartTrial.js
+++ src/screens/StartTrial.js
@@ -27,49 +27,49 @@ class StartTrial extends Component {
     return (
       <View style={localStyles.container2}>
         <ScrollView style={[localStyles.container]}>
-          <Text style={[styles.display2, localStyles.marginB]}>
-            <Translate keyName='caps_title1' />
+          <Text style={[styles.display2, localStyles.margin]}>
+            <Translate keyName='common.upperTitle' />
           </Text>
-          <Text style={[styles.title, localStyles.marginB]}>
-            <Translate keyName='before_lbl1_1' />
+          <Text style={[styles.title, localStyles.margin]}>
+            <Translate keyName='startTrial.weKnow' />
             <Text style={{fontWeight: 'bold'}}>
-              <Translate keyName='before_lbl1_2' />
+              <Translate keyName='startTrial.weWant' />
             </Text>
           </Text>
-          <Text style={[styles.title, localStyles.marginB]}>
-            <Translate keyName='before_lbl2' />
+          <Text style={[styles.title, localStyles.margin]}>
+            <Translate keyName='startTrial.receive' />
           </Text>
           <TouchableHighlight onPress={this.onPressSubsciption}
             style={[styles.button, styles.filledButton]}>
             <Text style={[styles.buttonText]}>
-              <Translate keyName='before_lbl7' />
+              <Translate keyName='startTrial.start' />
             </Text>
           </TouchableHighlight>
 
-          <Text style={[styles.body1, localStyles.marginB]}>
-            <Translate keyName='before_lbl3' />
+          <Text style={[styles.body1, localStyles.margin]}>
+            <Translate keyName='startTrial.price' />
           </Text>
 
           <TouchableHighlight onPress={this.onPressLogin}
-            style={[styles.button, styles.outlinedButton, localStyles.marginB]}>
+            style={[styles.button, styles.outlinedButton, localStyles.margin]}>
             <Text style={[styles.buttonText, styles.notColor]}>
-              <Translate keyName='before_lbl8' />
+              <Translate keyName='startTrial.logIn' />
             </Text>
           </TouchableHighlight>
 
-          <View style={[styles.separator, localStyles.marginB]} />
+          <View style={[styles.separator, localStyles.margin]} />
           <Text style={[styles.body2, {textAlign: 'left', marginBottom: height * 0.01}]}>
-            <Translate keyName='before_lbl4' />
+            <Translate keyName='startTrial.cancel' />
           </Text>
           <Text style={[styles.body1, {textAlign: 'left'}]}>
-            <Translate keyName='before_lbl5' />
+            <Translate keyName='startTrial.recurring' />
           </Text>
         </ScrollView>
         <View style={localStyles.container3}>
           <Text style={[styles.title, {textAlign: 'center'}]}>
-            <Translate keyName='before_lbl6_1' />
+            <Translate keyName='startTrial.already' />
             <Text style={[{fontWeight: 'bold', textDecorationLine: 'underline'}]}>
-              <Translate keyName='before_lbl6_2' />
+              <Translate keyName='startTrial.restore' />
             </Text>
           </Text>
         </View>
@@ -103,7 +103,7 @@ const localStyles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  marginB: {
+  margin: {
     marginBottom: height * 0.03,
   },
 })
