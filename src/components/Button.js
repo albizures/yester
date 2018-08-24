0be0af721974cac585bac0f53b857d0c71ed0802
gluix20 <lx.galvez@gmail.com>
2018-08-24 10:26:32 -0600

    <Button> as in specfile ans tests

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { TouchableHighlight, ViewPropTypes, Dimensions } from 'react-native'
-import LinearGradient from 'react-native-linear-gradient'
+import { View, TouchableHighlight, Image, ViewPropTypes } from 'react-native'
 import colors from '../utils/colors'
 import Translate from '../components/Translate'
 
-const gradientColors = [colors.gradient1, colors.gradient2]
-
 const Button = (props) => {
+  var specificButton = styles.filledButton
+  var specificText = styles.filledButtonText
+  var specificIcon = styles.filledButtonIcon
+  var icon
+
+  if (props.type !== 'filled') {
+    specificButton = styles.outlinedButton
+    specificText = styles.outlinedButtonText
+    specificIcon = styles.outlinedButtonIcon
+  }
+  if (props.icon !== 0) {
+    specificText.paddingLeft = 9
+    icon = <Image source={props.icon} style={[styles.icon, specificIcon]} />
+  }
+  if (props.size === 'small') {
+    specificButton.width = 150
+  }
+
   return (
-    <TouchableHighlight {...props} elevation={10} style={[props.style, styles.button]}>
-      <LinearGradient start={{x: 0.0, y: 1.0}} end={{x: 1.0, y: 1.0}} style={styles.gradient} colors={gradientColors}>
-        <Translate style={styles.text} keyName={props.title} />
-      </LinearGradient>
+    <TouchableHighlight {...props} elevation={10}
+      style={[props.style, styles.button, specificButton]}>
+      <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+        {icon}
+        <Translate style={[styles.text, specificText]} keyName={props.title} />
+      </View>
     </TouchableHighlight>
   )
 }
@@ -20,33 +37,49 @@ const Button = (props) => {
 Button.propTypes = {
   style: ViewPropTypes.style,
   title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  icon: PropTypes.number.isRequired,
+  size: PropTypes.string.isRequired,
 }
 
-let { height } = Dimensions.get('window')
 const styles = {
-  gradient: {
-    flex: 1,
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: height * 0.04,
-  },
   button: {
     height: 50,
-    shadowColor: '#000000',
+    width: 300,
+    borderRadius: 24,
+    borderWidth: 2,
+    justifyContent: 'center',
+    shadowColor: colors.black,
     shadowOffset: {
       width: 0,
       height: 3,
     },
-    borderRadius: height * 0.04,
-    // backgroundColor: '#b98a56',
-    width: '100%',
+  },
+  filledButton: {
+    backgroundColor: colors.governorBay,
+    borderColor: colors.governorBay,
+  },
+  outlinedButton: {
+    backgroundColor: colors.white,
+    borderColor: colors.governorBay,
   },
   text: {
-    // marginVertical: 20,
-    color: '#ffffff',
-    fontWeight: 'bold',
     fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  filledButtonText: {
+    color: colors.white,
+  },
+  outlinedButtonText: {
+    color: colors.governorBay,
+  },
+  icon: {width: 20, height: 20},
+  filledButtonIcon: {
+    tintColor: colors.white,
+  },
+  outlinedButtonIcon: {
+    tintColor: colors.governorBay,
   },
 }
 
--- src/components/__tests__/Button.js
+++ src/components/__tests__/Button.js
@@ -6,7 +6,14 @@ import Button from '../Button'
 
 describe('src/components/Button.js', () => {
   it('should render a TouchableHighlight', () => {
-    const shallowButton = shallow(<Button title='Button' />)
+    const shallowButton = shallow(<Button title='Button' type={'filled'} size={'normal'} icon={0} />)
     expect(shallowButton.type()).toBe(TouchableHighlight)
+  })
+
+  describe('when size button is small', () => {
+    it('should render a TouchableHighlight', () => {
+      const shallowButton = shallow(<Button title='Button' type={'filled'} size={'small'} icon={0} />)
+      expect(shallowButton.type()).toBe(TouchableHighlight)
+    })
   })
 })
