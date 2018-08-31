88acb3cf8aa526a955720106a018fca89a14662a
gluix20 <lx.galvez@gmail.com>
2018-08-30 23:14:57 -0600

    Create QuestionCard

--- src/screens/Question.js
+++ src/screens/Question.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Text, View, Button } from 'react-native'
+import QuestionCard from './Home/QuestionCard'
 
 export default class Question extends Component {
   static propTypes = {
@@ -16,6 +17,9 @@ export default class Question extends Component {
       <View>
         <Text style={[{textAlign: 'center', marginTop: 40}]}>QUESTION</Text>
         <Button title='to Writing' onPress={this.onPress} />
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+          <QuestionCard text={'Question Card'} />
+        </View>
       </View>
     )
   }
