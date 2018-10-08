077edf2c0ecc466b0d0b8011461e8c4eb43bcd10
gluix20 <lx.galvez@gmail.com>
2018-10-08 12:55:25 -0600

    Add input to signin

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Text, View, Button } from 'react-native'
+// import colors from '../utils/colors'
+import CustomTextInput from '../components/CustomTextInput'
 
 export default class SignIn extends Component {
   static propTypes = {
@@ -13,9 +15,10 @@ export default class SignIn extends Component {
 
   render () {
     return (
-      <View>
+      <View style={{alignItems: 'center'}}>
         <Text style={[{textAlign: 'center', marginTop: 40}]}>SIGN IN</Text>
         <Button title='to Home' onPress={this.onPress} />
+        <CustomTextInput title='signin.firstName' />
       </View>
     )
   }
