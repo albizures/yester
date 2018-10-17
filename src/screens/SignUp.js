1376cbd02a728fb4f2caadc9943d62b2fbe449e9
gluix20 <lx.galvez@gmail.com>
2018-10-17 10:47:49 -0600

    Improving style to SignUp screen

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -5,9 +5,10 @@ import { Auth } from 'aws-amplify'
 
 import Container from '../components/Container'
 import Button from '../components/Button'
-import Translate from '../components/Translate'
+import { Heading2, Heading4 } from '../components/Translate'
 import TopBar from '../components/TopBar'
 import TextInput from '../components/TextInput'
+import colors from '../utils/colors'
 
 export default class SignUp extends Component {
   static propTypes = {
@@ -65,8 +66,10 @@ export default class SignUp extends Component {
     )
     return (
       <Container scroll topBar={topBar}>
-        <Translate keyName='signup.title' style={[{textAlign: 'center', marginTop: 40}]} />
-        <Translate keyName='signup.subtitle' style={[{textAlign: 'center', marginTop: 40}]} />
+        <View style={{height: 39, justtifyContent: 'flex-start', marginTop: 40}}>
+          <Heading2 keyName='signup.title' style={[{textAlign: 'center', color: colors.governorBay}]} />
+        </View>
+        <Heading4 keyName='signup.subtitle' style={[{textAlign: 'center', marginBottom: 40, marginHorizontal: 33}]} />
         <View style={{alignItems: 'center'}}>
           <TextInput title='signup.firstName'
             value={firstName} onChangeText={text => this.onChange(text, 'firstName')} />
@@ -76,7 +79,7 @@ export default class SignUp extends Component {
             value={email} onChangeText={text => this.onChange(text, 'email')} />
           <TextInput title='signup.password' password
             value={password} onChangeText={text => this.onChange(text, 'password')} />
-          <Button title='signup.submit' onPress={this.onPress} />
+          <Button title='signup.submit' onPress={this.onPress} style={{marginTop: 15}} />
         </View>
       </Container>
     )
