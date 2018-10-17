19357c6ba0fa8d895e756993bd423f18769104d6
Jose Albizures <albizures3601@gmail.com>
2018-10-16 19:29:33 -0600

    redirect to setup after confirm account

--- src/screens/ConfirmAccount.js
+++ src/screens/ConfirmAccount.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { View, Alert, StyleSheet, Text } from 'react-native'
+import { View, Alert, StyleSheet } from 'react-native'
 import { Auth } from 'aws-amplify'
+
 import Translate from '../components/Translate'
 import Container from '../components/Container'
 import Button from '../components/Button'
 import TextInput from '../components/TextInput'
+import { logIn } from '../utils/session'
 
 export default class ConfirmAccount extends Component {
   static propTypes = {
@@ -26,13 +28,26 @@ export default class ConfirmAccount extends Component {
     })
   }
 
+  async postConfirm () {
+    const { navigation } = this.props
+    const email = navigation.getParam('email')
+    const password = navigation.getParam('password')
+    try {
+      await logIn(email, password)
+      navigation.navigate('Setup')
+    } catch (error) {
+      navigation.navigate('Login')
+    }
+  }
+
   onPress = async () => {
     const { navigation } = this.props
     const email = navigation.getParam('email')
+
     const { code } = this.state
     try {
       await Auth.confirmSignUp(email, code)
-      navigation.navigate('Setup')
+      this.postConfirm()
     } catch (error) {
       console.log('ConfirmAccount', error)
       Alert.alert(error.message || error)
@@ -50,8 +65,7 @@ export default class ConfirmAccount extends Component {
           <Translate keyName='confirm.title' />
           <Translate keyName='confirm.subtitle' />
           <View>
-            <Translate keyName='confirm.label' />
-            <Text>{email || number}</Text>
+            <Translate keyName='confirm.label' data={{contact: email || number}} />
           </View>
           <TextInput title='confirm.inputLabel' keyboardType='numeric'
             value={code} onChangeText={text => this.onChange(text, 'code')} />
