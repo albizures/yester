678dc79c1206f0162af52380ea701dce7598ae06
gluix20 <lx.galvez@gmail.com>
2018-10-17 15:03:33 -0600

    Change Heading1, Heading2, ... to high end function component

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -11,43 +11,13 @@ const strings = new LocalizedStrings({
   es: require('../translations/es'),
 })
 
-const fontColor = colors.mineShaft
-
 const Translate = ({ keyName, data, ...props }) => (
   <Text {...props}>{suppplant(strings[keyName], data)}</Text>
 )
 
-export const Heading1 = ({ style, ...props }) => (
-  <Translate {...props} style={[styles.heading1].concat(style)} />
+const createStyledTranslation = (styled) => ({ style, ...props }) => (
+  <Translate {...props} style={[styles.text].concat(styled).concat(style)} />
 )
-
-export const Heading2 = ({ style, ...props }) => (
-  <Translate {...props} style={[styles.heading2].concat(style)} />
-)
-
-export const Heading3 = ({ style, ...props }) => (
-  <Translate {...props} style={[styles.heading3].concat(style)} />
-)
-
-export const Heading4 = ({ style, ...props }) => (
-  <Translate {...props} style={[styles.heading4].concat(style)} />
-)
-
-export const Heading5 = ({ style, ...props }) => (
-  <Translate {...props} style={[styles.heading5].concat(style)} />
-)
-
-export const Title = ({ style, ...props }) => (
-  <Translate {...props} style={[styles.title].concat(style)} />
-)
-
-export const Description = ({ style, ...props }) => (
-  <Translate {...props} style={[styles.description].concat(style)} />
-)
-
-Title.propTypes = {
-  style: Text.propTypes.style,
-}
 
 Translate.propTypes = {
   keyName: PropTypes.string.isRequired,
@@ -55,51 +25,42 @@ Translate.propTypes = {
 }
 
 const styles = StyleSheet.create({
-  heading1: {
+  text: {
     fontFamily: 'Karla-Regular',
-    color: fontColor,
+    color: colors.mineShaft,
+    textAlign: 'left',
+  },
+  heading1: {
     fontSize: 50,
     fontWeight: 'bold',
-    textAlign: 'left',
   },
   heading2: {
-    fontFamily: 'Karla-Regular',
-    color: fontColor,
     fontSize: 25,
     fontWeight: 'bold',
-    textAlign: 'left',
   },
   heading3: {
-    fontFamily: 'Karla-Regular',
-    color: fontColor,
     fontSize: 18,
     fontWeight: 'bold',
-    textAlign: 'left',
   },
   heading4: {
-    fontFamily: 'Karla-Regular',
-    color: fontColor,
     fontSize: 18,
-    textAlign: 'left',
   },
   heading5: {
-    fontFamily: 'Karla-Regular',
-    color: fontColor,
     fontSize: 16,
-    textAlign: 'left',
   },
   title: {
-    fontFamily: 'Karla-Regular',
-    color: fontColor,
     fontSize: 20,
-    textAlign: 'left',
   },
   description: {
-    fontFamily: 'Karla-Regular',
-    color: fontColor,
     fontSize: 12,
-    textAlign: 'left',
   },
 })
 
 export default Translate
+export const Heading1 = createStyledTranslation(styles.heading1)
+export const Heading2 = createStyledTranslation(styles.heading2)
+export const Heading3 = createStyledTranslation(styles.heading3)
+export const Heading4 = createStyledTranslation(styles.heading4)
+export const Heading5 = createStyledTranslation(styles.heading5)
+export const Title = createStyledTranslation(styles.title)
+export const Description = createStyledTranslation(styles.description)
