a717ced19da32d6d4657ffc215f9d47a61c82c5a
gluix20 <lx.galvez@gmail.com>
2018-10-17 10:40:55 -0600

    Adding translations, some temporal ilustration assets

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -4,6 +4,9 @@ const icons = {
   chevronRight: require('../assets/chevron/chevron-right.png'),
   chevronDown: require('../assets/chevron/chevron-down.png'),
   buttonPlus: require('../assets/plus-circle.png'),
+  ssSonaja: require('../assets/screenshots/ss-sonaja.png'),
+  ssGlobo: require('../assets/screenshots/ss-globo.png'),
+  ssCard: require('../assets/screenshots/ss-card.png'),
 }
 
 export default icons
