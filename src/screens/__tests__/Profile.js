9b7bb0db55ed52ce3fe8fee0961dfdd901008e5e
gluix20 <lx.galvez@gmail.com>
2018-10-22 18:03:14 -0600

    Update snapshots

--- src/screens/__tests__/Profile.js
+++ src/screens/__tests__/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import renderer from 'react-test-renderer'
-import { Button } from 'react-native'
+import { Container } from '../../components/Container'
 
 import Profile from '../Profile'
 
@@ -11,18 +11,8 @@ describe('src/screens/Profile.js', () => {
   it('should render', () => {
     const tree = renderer.create(<Profile navigation={navigation} />).toJSON()
     expect(tree).toMatchSnapshot()
-  })
 
-  describe('when button to home is pressed', () => {
-    it('should navigate to Home screen', () => {
-      const shallowProfile = shallow(<Profile navigation={navigation} />)
-      const shallowTouchableHighlights = shallowProfile.find(Button)
-      const shallowButtonToHome = shallowTouchableHighlights.at(0)
-
-      shallowButtonToHome.prop('onPress')()
-
-      expect(navigation.navigate).toHaveBeenCalledTimes(1)
-      expect(navigation.navigate).toHaveBeenCalledWith('Home')
-    })
+    const shallowHome = shallow(<Profile navigation={navigation} />)
+    expect(shallowHome.type).toBe(Container)
   })
 })
