4fd52e36a0d5eea8c6b849a6d04b3d0ee5d5c7bf
gluix20 <lx.galvez@gmail.com>
2018-11-04 12:40:03 -0600

    Installing and linking react-native-purchases library

--- android/app/src/main/java/com/puchicapps/yester/MainApplication.java
+++ android/app/src/main/java/com/puchicapps/yester/MainApplication.java
@@ -3,6 +3,7 @@ package com.puchicapps.yester;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactlibrary.RNPurchasesPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.amazonaws.RNAWSCognitoPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNPurchasesPackage(),
             new FBSDKPackage(),
             new RNAWSCognitoPackage(),
             new FBSDKPackage(mCallbackManager),
