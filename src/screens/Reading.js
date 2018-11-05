70ad8dd8fe8c1e726e6597d8c491bec9006a1495
Jose Albizures <albizures3601@gmail.com>
2018-11-04 18:47:00 -0600

    show an story

--- src/screens/Reading.js
+++ src/screens/Reading.js
@@ -1,49 +1,99 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { View, Text, Button } from 'react-native'
-import {Heading1, Description} from '../components'
+import { View, Text, StyleSheet, Share } from 'react-native'
+import { Heading1, Description, Title } from '../components'
 import Container from '../components/Container'
 import Divider from '../components/Divider'
 
 import TopBar from '../components/TopBar'
 import colors from '../utils/colors'
+import http from '../utils/http'
 
 export default class Reading extends Component {
   static propTypes = {
     navigation: PropTypes.object.isRequired,
   }
 
-  onPress = () => {
-    this.props.navigation.navigate('Home')
+  state = {
+    isLoading: false,
+    title: '',
+    content: '',
+  }
+
+  async componentDidMount () {
+    const { navigation } = this.props
+    const storyId = navigation.getParam('storyId')
+
+    this.setState({ isLoading: true })
+
+    try {
+      const { data: story } = await http.get(`/v1/stories/${storyId}`)
+      this.setState(story)
+    } catch (error) {
+      console.log(error)
+      console.log(error.message)
+      console.log(error.response)
+    }
+
+    this.setState({ isLoading: false })
+  }
+
+  onBack = () => {
+    this.props.navigation.goBack()
+  }
+
+  onShare = () => {
+    const { title, content } = this.state
+    const message = `${title}: ${content}`.substr(0, 250)
+    Share.share({
+      title: 'test lwlw',
+      message: `${message}...`,
+    })
   }
 
   render () {
+    const { isLoading, title, content } = this.state
+
+    const action = (
+      <Title
+        keyName='reading.share'
+        style={{ textDecorationLine: 'underline', color: colors.governorBay }}
+        onPress={this.onShare} />
+    )
+
     const topBar = (
-      <TopBar title='reading.topbar' style={{backgroundColor: colors.white}} />
+      <TopBar
+        transparent
+        onBack={this.onBack}
+        title='reading.topbar'
+        action={action} />
     )
     return (
-      <Container topBar={topBar} style={{backgroundColor: colors.athensGray}}>
-        <View style={{backgroundColor: colors.athensGray, paddingHorizontal: 29}}>
-          <Button title='to Home' onPress={this.onPress} />
-          <Heading1 text='Title' style={{color: colors.governorBay}} />
+      <Container isLoading={isLoading} topBar={topBar}>
+        <View style={styles.container}>
+          <Heading1 text={title} style={{color: colors.governorBay}} />
           <Description keyName='writing.by' data={{autor: 'Luis Galvez'}} />
           <Description text='{month} {day}, {year}' data={{month: 'July', day: '6', year: '1987'}} />
-          <Divider style={{marginVertical: 30}} />
-          <Text maxLength={1000} multiline
-            style={{
-              height: 400,
-              fontFamily: 'Karla-Regular',
-              fontSize: 16,
-              paddingHorizontal: 15,
-              paddingTop: 15,
-              textAlignVertical: 'top',
-              textAlign: 'justify',
-              backgroundColor: colors.white,
-              marginTop: 30,
-            }}
-          />
+          <Divider style={{marginVertical: 40, marginHorizontal: 0}} />
+          <Text maxLength={1000} multiline style={styles.content}>
+            {content}
+          </Text>
         </View>
       </Container>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 29,
+  },
+  content: {
+    height: 400,
+    fontFamily: 'Karla-Regular',
+    fontSize: 18,
+    textAlignVertical: 'top',
+    textAlign: 'justify',
+    backgroundColor: 'transparent',
+  },
+})
