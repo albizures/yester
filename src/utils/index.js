cb13fdaf34f13d5dd4ee49bfc79305de82117ba2
Jose Albizures <albizures3601@gmail.com>
2018-11-11 17:09:40 -0600

    add default value for capitalize

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 export const indexToString = (item, index) => index.toString()
 
-export const capitalize = (text) => text.charAt(0).toUpperCase() + text.substr(1)
+export const capitalize = (text = '') => text.charAt(0).toUpperCase() + text.substr(1)
 
 export const extractSetupParams = (navigation) => ({
   birthDate: navigation.getParam('birthDate'),
