732a014adcdf42fab4e53ecf7bbac8b7b4ca7852
gluix20 <lx.galvez@gmail.com>
2018-11-20 11:53:09 -0600

    Add withUser to FBWebView to set locale attibute

--- src/screens/FBWebView.js
+++ src/screens/FBWebView.js
@@ -2,31 +2,34 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { View, StyleSheet, WebView, Alert } from 'react-native'
 import { FACEBOOK_URL_LOGIN } from 'react-native-dotenv'
+import withUser, { shapeContextUser } from '../components/withUser'
+import { strings } from '../components/Translate'
+import { loginWithFBWebView, updateUserAttribute } from '../utils/session'
 
-import { loginWithFBWebView } from '../utils/session'
-
-export default class FBWebView extends React.Component {
+class FBWebView extends React.Component {
   static propTypes = {
     navigation: PropTypes.object.isRequired,
+    contextUser: PropTypes.shape(shapeContextUser).isRequired,
   }
 
   onFBWebViewStateChange = async (event) => {
+    const { updateUser } = this.props.contextUser
     const { navigation } = this.props
     console.log('onFBWebViewStateChange', event.url)
-    if (event.url.includes('https://www.yester.app')) {
+    if (event.url.startsWith('https://www.yester.app')) {
       if (event.url.includes('access_token=')) {
         try {
           await loginWithFBWebView(event.url)
+          await updateUserAttribute('locale', strings.getLanguage())
+          await updateUser()
           return navigation.navigate('AppLoading')
         } catch (error) {
           console.log('onFBWebViewStateChange', error)
-          Alert.alert('Error', JSON.stringify(error))
+          navigation.goBack()
           Alert.alert('Error', JSON.stringify(error.message))
         }
       }
     }
-
-    navigation.goBack()
   }
 
   render () {
@@ -52,3 +55,5 @@ const styles = StyleSheet.create({
     width: '100%',
   },
 })
+
+export default withUser(FBWebView)
