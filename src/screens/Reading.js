1f7fc8974f0581e71ba824398728afbceed22874
Jose Albizures <albizures3601@gmail.com>
2018-11-23 23:12:31 -0600

    use different text sizes base on the text length

--- src/screens/Reading.js
+++ src/screens/Reading.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { View, Text, StyleSheet, Share } from 'react-native'
 
-import { Heading1, Description, Title } from '../components'
+import { Heading1, Description, Title, Heading2 } from '../components'
 import Container from '../components/Container'
 import TopBar from '../components/TopBar'
 import withUser, { shapeContextUser } from '../components/withUser'
@@ -69,6 +69,15 @@ class Reading extends Component {
     })
   }
 
+  getTitleComponent () {
+    const { title } = this.state
+    if (title.length > 65) {
+      return Heading2
+    }
+
+    return Heading1
+  }
+
   render () {
     const { isLoading, title, content, ageId, created } = this.state
     const { name: author } = this.props.contextUser.user
@@ -88,12 +97,15 @@ class Reading extends Component {
         onBack={this.onBack}
         action={action} />
     )
+
+    const TitleComponent = this.getTitleComponent()
+
     return (
       <Container scroll isLoading={isLoading} topBar={topBar}>
         <View style={styles.container}>
           {/* TODO: add logo */}
           <Description text={age.toUpperCase()} />
-          <Heading1 text={title} style={{color: colors.governorBay, marginTop: 40}} />
+          <TitleComponent text={title} style={{color: colors.governorBay, marginTop: 40}} />
           {/* TODO: make smaller this font */}
           <Description keyName='reading.by' data={{ author }} />
           {/* TODO: make smaller this font and changes its color to gray */}
