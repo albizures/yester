878fec9f046cf2c1cb707c69fb4e537b7c26ba58
Jose Albizures <albizures3601@gmail.com>
2018-12-04 19:41:10 -0600

    update gradle to 4.6

--- android/app/src/main/java/com/puchicapps/yester/MainApplication.java
+++ android/app/src/main/java/com/puchicapps/yester/MainApplication.java
@@ -3,6 +3,7 @@ package com.puchicapps.yester;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.amazonaws.RNAWSCognitoPackage;
 import com.reactlibrary.RNPurchasesPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.amazonaws.RNAWSCognitoPackage;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNAWSCognitoPackage(),
             new RNPurchasesPackage(),
             new RNAWSCognitoPackage(),
             new FBSDKPackage(mCallbackManager),
