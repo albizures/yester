82fe722e3ef66d69481544f0c4ee719a2aa6416e
gluix20 <lx.galvez@gmail.com>
2019-03-23 17:33:04 -0600

    Changing Subscription screen

--- src/screens/Subscription.js
+++ src/screens/Subscription.js
@@ -1,31 +1,14 @@
 import React, { Component } from 'react'
-import {
-  StyleSheet,
-  View,
-  Image,
-  Dimensions,
-  Alert,
-  Text,
-  Linking,
-} from 'react-native'
+import { StyleSheet, View, Image, Dimensions, Alert, Text, Linking } from 'react-native'
 import PropTypes from 'prop-types'
 import colors from '../utils/colors'
 import icons from '../utils/icons'
 import Container from '../components/Container'
-import { Title, Description, Heading1, Heading5 } from '../components'
+import { Title, Description, Heading1, Heading5, Heading3, Heading4, Body1 } from '../components'
 import Button, { types } from '../components/Button'
 import Divider from '../components/Divider'
-import {
-  saveUserSubscriptionStatus,
-  removeSuscription,
-  logOut,
-} from '../utils/session'
-import {
-  getEntitlements,
-  buySubscription,
-  status,
-  setPurchaseListener,
-} from '../utils/purchase'
+import { saveUserSubscriptionStatus, removeSuscription, logOut } from '../utils/session'
+import { getEntitlements, buySubscription, status, setPurchaseListener } from '../utils/purchase'
 
 class Subscription extends Component {
   static propTypes = {
@@ -60,9 +43,7 @@ class Subscription extends Component {
     if (error) {
       console.log('error', error)
       const message =
-        error.code === 2
-          ? 'Process has been cancelled'
-          : "We couldn't process your payment"
+        error.code === 2 ? 'Process has been cancelled' : "We couldn't process your payment"
       return Alert.alert(message)
     }
 
@@ -107,29 +88,18 @@ class Subscription extends Component {
         <Image source={icons.subscription} style={styles.image} />
         <Container scroll style={styles.container}>
           <View style={styles.topFlex}>
-            <Title
-              keyName='subscription.close'
-              style={styles.closeText}
-              onPress={this.onLogOut}
-            />
+            <Title keyName='subscription.close' style={styles.closeText} onPress={this.onLogOut} />
             <Heading1 keyName='subscription.try' style={styles.tryText} />
-            <Description
-              keyName='subscription.price'
-              style={styles.priceText}
-            />
-            <Image source={icons.ballon} style={styles.ballonImage} />
-            <Title keyName='subscription.slogan' style={styles.sloganText} />
-            <Title
-              keyName='subscription.features'
-              style={styles.featuresText}
-            />
+            <Heading5 keyName='subscription.price' style={styles.priceText} />
+            {
+              // <Image source={icons.ballon} style={styles.ballonImage} />
+            }
+            <Heading3 keyName='subscription.slogan' style={styles.sloganText} />
+            <Heading4 keyName='subscription.features' style={styles.featuresText} />
 
-            <Button
-              title='subscription.start'
-              onPress={this.onStartTrial}
-              type={types.OUTLINED}
-            />
-            <Heading5
+            <Button title='subscription.start' onPress={this.onStartTrial} type={types.OUTLINED} />
+            <Body1 keyName='subscription.priceAfter' style={styles.priceAfterText} />
+            <Body1
               keyName='subscription.restore'
               style={styles.restoreText}
               onPress={this.onStartTrial}
@@ -139,24 +109,15 @@ class Subscription extends Component {
           <View style={styles.bottomFlex}>
             <Divider style={styles.divider} />
 
-            <Description
-              keyName='subscription.termsTitle'
-              style={styles.termsTitleText}
-            />
+            <Description keyName='subscription.termsTitle' style={styles.termsTitleText} />
             <Text style={{ paddingBottom: 40 }}>
-              <Description
-                keyName='subscription.terms'
-                style={styles.termsText}
-              />
+              <Description keyName='subscription.terms' style={styles.termsText} />
               <Description
                 keyName='subscription.termsLink'
                 style={styles.termsLink}
                 onPress={this.onPressTerms}
               />
-              <Description
-                keyName='subscription.termsAnd'
-                style={styles.termsText}
-              />
+              <Description keyName='subscription.termsAnd' style={styles.termsText} />
               <Description
                 keyName='subscription.privacyLink'
                 style={styles.termsLink}
@@ -213,8 +174,9 @@ const styles = StyleSheet.create({
   },
   priceText: {
     color,
+    fontWeight: 'bold',
     textAlign,
-    marginBottom: height * 0.03,
+    marginBottom: height * 0.07,
   },
   ballonImage: {
     width: 78,
@@ -225,19 +187,23 @@ const styles = StyleSheet.create({
     color,
     textAlign,
     fontWeight: 'bold',
-    marginBottom: height * 0.03,
+    marginBottom: height * 0.02,
   },
   featuresText: {
     color,
     textAlign,
     marginBottom: height * 0.05,
   },
+  priceAfterText: {
+    color,
+    textAlign,
+    marginTop: height * 0.01,
+  },
   restoreText: {
     color,
     textAlign,
-    fontWeight: 'bold',
     textDecorationLine: 'underline',
-    marginTop: height * 0.03,
+    marginTop: height * 0.04,
   },
   termsTitleText: {
     textAlign: 'left',
