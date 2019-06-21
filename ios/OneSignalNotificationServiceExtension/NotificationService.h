9909bfbf9bc05dafb22977c9ea23b9520b72b01c
gluix20 <lx.galvez@gmail.com>
2019-06-20 23:03:03 -0600

    Creating OneSignalNotificationServiceExtension tgt

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Luis Galvez on 20/6/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
