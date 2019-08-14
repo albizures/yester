ed68708b32bf0aa3820c03d13ef75f9afe565a8b
gluix20 <lx.galvez@gmail.com>
2019-08-13 23:16:05 -0600

    Getting first and last name strings

--- src/utils/fbHelper.js
+++ src/utils/fbHelper.js
@@ -1,19 +1,24 @@
 import { GraphRequest, GraphRequestManager } from 'react-native-fbsdk'
 
-export const getCurrentProfile = () => new Promise((resolve, reject) => {
-  const req = new GraphRequest('/me', {
-    httpMethod: 'GET',
-    version: 'v2.5',
-    parameters: {
-      fields: {
-        string: 'email,name,cover',
+export const getCurrentProfile = () =>
+  new Promise((resolve, reject) => {
+    const req = new GraphRequest(
+      '/me',
+      {
+        httpMethod: 'GET',
+        version: 'v2.5',
+        parameters: {
+          fields: {
+            string: 'email,name,cover,first_name,last_name,locale',
+          },
+        },
       },
-    },
-  }, (err, res) => {
-    if (err) {
-      return reject(err)
-    }
-    resolve(res)
+      (err, res) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve(res)
+      }
+    )
+    new GraphRequestManager().addRequest(req).start()
   })
-  new GraphRequestManager().addRequest(req).start()
-})
