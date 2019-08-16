639de158f917b596e4a391ad61b8ef6a84ee6030
gluix20 <lx.galvez@gmail.com>
2019-08-16 12:45:10 -0600

    Installing RN-device-info

--- android/app/src/main/java/com/yester/app/MainApplication.java
+++ android/app/src/main/java/com/yester/app/MainApplication.java
@@ -3,6 +3,7 @@ package com.yester.app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.segment.analytics.reactnative.core.RNAnalyticsPackage;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
@@ -39,6 +40,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new RNAnalyticsPackage(),
             new ReactNativeOneSignalPackage(),
             new RNCWebViewPackage(),
