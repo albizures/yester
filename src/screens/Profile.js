93b6c442004c757f42d697cb125ea8b15db6660a
gluix20 <lx.galvez@gmail.com>
2019-08-19 17:17:53 -0600

    Adding confirm email to Profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { View, Image, Dimensions, StyleSheet } from 'react-native'
-import { Heading2, Description, Heading3 } from '../components'
+import { Heading2, Description, Heading5 } from '../components'
 import Container from '../components/Container'
 import TopBar from '../components/TopBar'
 import Divider from '../components/Divider'
@@ -17,10 +17,19 @@ class Profile extends Component {
     navigation: PropTypes.object.isRequired,
   }
 
+  constructor (props) {
+    super()
+    const {
+      contextUser: { user },
+    } = props
+    this.state = { user }
+  }
+
   willFocusListener = null
 
   componentDidMount () {
-    const { addListener } = this.props.navigation
+    const { navigation } = this.props
+    const { addListener } = navigation
     this.willFocusListener = addListener('willFocus', this.load)
   }
 
@@ -33,20 +42,46 @@ class Profile extends Component {
   }
 
   onPressEdit = async () => {
-    const {
-      navigation,
-      contextUser: { user },
-    } = this.props
+    const { navigation } = this.props
+    const { user } = this.state
     const { params } = await isSetupFinished(user)
     return navigation.navigate('SetupBirthDate', params)
   }
 
+  onPressVerify = async () => {
+    const { navigation } = this.props
+    const { email, emailVerified } = this.state.user
+    if (!emailVerified) {
+      return navigation.navigate('ConfirmAccount', { email: email, signUpVerify: false })
+    }
+  }
+
   render () {
-    const { name, email, country, state, gender, birthPlace } = this.props.contextUser.user
+    const { name, email, country, state, gender, birthPlace, emailVerified } = this.state.user
     const location =
       birthPlace !== undefined ? birthPlace : `${country}, ${state ? state.substring(3, 5) : ''}`
 
     const topBar = <TopBar title='profile.title' />
+
+    let verificationElement = (
+      <Description
+        keyName='profile.verifiedEmail.confirmed'
+        style={{ color: 'green', marginTop: height * 0.01 }}
+      />
+    )
+    if (!emailVerified) {
+      verificationElement = (
+        <View style={styles.verification}>
+          <Description keyName='profile.verifiedEmail.unconfirmed' style={{ color: 'red' }} />
+          <Heading5
+            keyName='profile.verifiedEmail.confirm'
+            style={styles.confirmText}
+            onPress={this.onPressVerify}
+          />
+        </View>
+      )
+    }
+
     return (
       <Container topBar={topBar} style={styles.container}>
         <View style={styles.topFlex}>
@@ -65,12 +100,13 @@ class Profile extends Component {
           <Divider style={{ width: 323 }} />
           <View style={styles.item}>
             <Description keyName='profile.email' />
-            <Heading3 text={email} />
+            <Heading5 text={email} style={{ fontWeight: 'bold' }} onPress={this.onPressVerify} />
+            {verificationElement}
           </View>
           <Divider style={{ width: 323 }} />
           <View style={styles.item}>
             <Description keyName='profile.location' />
-            <Heading3 text={location} />
+            <Heading5 text={location} style={{ fontWeight: 'bold' }} />
           </View>
           <Divider style={{ width: 323 }} />
           <View style={styles.edit}>
@@ -113,9 +149,20 @@ const styles = StyleSheet.create({
   },
   item: {
     width: 300,
-    height: 80,
+    minHeight: height * 0.1,
     alignItems: 'flex-start',
-    paddingVertical: 20,
+    paddingVertical: height * 0.025,
+  },
+  verification: {
+    flexDirection: 'row',
+    width: 300,
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+    marginTop: height * 0.015,
+  },
+  confirmText: {
+    fontWeight: 'bold',
+    color: colors.governorBay,
   },
   edit: {
     width: 300,
