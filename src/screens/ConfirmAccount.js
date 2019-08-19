73e512c241a427f0102c32a2e1d7e1abaefedd10
gluix20 <lx.galvez@gmail.com>
2019-08-19 01:24:30 -0600

    Adapting ConfirmAccount to new verification flow

--- src/screens/ConfirmAccount.js
+++ src/screens/ConfirmAccount.js
@@ -7,18 +7,17 @@ import {
   Image,
   KeyboardAvoidingView,
   Dimensions,
-  Platform,
+  TouchableOpacity,
 } from 'react-native'
 import { Auth } from 'aws-amplify'
-import { Body1, Heading2, Heading3, Heading4 } from '../components'
+import { Body1, Heading2, Heading3, Heading4, Description } from '../components'
 import Container from '../components/Container'
 import TopBar from '../components/TopBar'
 import Button from '../components/Button'
 import TextInput from '../components/TextInput'
-import { logIn, postAPIUser } from '../utils/session'
+import { updateUserAttribute } from '../utils/session'
 import colors from '../utils/colors'
 import icons from '../utils/icons'
-import DeviceInfo from 'react-native-device-info'
 import debugFactory from 'debug'
 
 const debugError = debugFactory('yester:ConfirmAccount:error')
@@ -31,10 +30,24 @@ export default class ConfirmAccount extends Component {
 
   constructor () {
     super()
+    Auth.verifyCurrentUserAttribute('email')
     this.state = {
       code: '',
-      email: 'albizures',
     }
+  }
+
+  componentDidMount () {
+    const { navigation } = this.props
+    const email = navigation.getParam('email')
+    const number = navigation.getParam('number')
+    const signUpVerify = navigation.getParam('signUpVerify')
+
+    this.setState({
+      code: '',
+      email,
+      number,
+      signUpVerify,
+    })
   }
 
   onChange = (value, name) => {
@@ -43,55 +56,38 @@ export default class ConfirmAccount extends Component {
     })
   }
 
-  async postConfirm () {
+  onPressContinue = async () => {
     const { navigation } = this.props
-    const email = navigation.getParam('email')
-    const password = navigation.getParam('password')
-    try {
-      await logIn(email, password)
-
-      // TODO Find the best way to allow manual name update.
-      const currentUser = await Auth.currentAuthenticatedUser()
-      await postAPIUser({
-        email: currentUser.attributes.email,
-        given_name: currentUser.attributes['given_name'],
-        family_name: currentUser.attributes['family_name'],
-        locale: currentUser.attributes['locale'],
-        platform: Platform.OS,
-        build: DeviceInfo.getBuildNumber(),
-        version: DeviceInfo.getVersion(),
-      })
-      return navigation.navigate('AppLoading')
-    } catch (error) {
-      navigation.navigate('Login')
-    }
-  }
-
-  onPress = async () => {
-    const { navigation } = this.props
-    const email = navigation.getParam('email')
-
     const { code } = this.state
+
     try {
-      await Auth.confirmSignUp(email, code)
-      this.postConfirm()
+      await Auth.verifyCurrentUserAttributeSubmit('email', code)
+      await updateUserAttribute('email_verified', true)
+      return navigation.navigate('AppLoading')
     } catch (error) {
       debugError('ConfirmAccount', error)
       Alert.alert(error.message || error)
     }
   }
 
-  onPressResend () {
+  onPressSkip = async () => {
     const { navigation } = this.props
-    const email = navigation.getParam('email')
-    Auth.resendSignUp({ username: email })
+    return navigation.navigate('AppLoading')
+  }
+
+  onPressResend () {
+    Auth.verifyCurrentUserAttribute('email')
   }
 
   render () {
-    const { code } = this.state
-    const { navigation } = this.props
-    const email = navigation.getParam('email')
-    const number = navigation.getParam('number')
+    const { code, email, number, signUpVerify } = this.state
+
+    const skipElement = (
+      <TouchableOpacity>
+        <Description keyName='confirm.skip' style={styles.skipLabel} onPress={this.onPressSkip} />
+      </TouchableOpacity>
+    )
+
     const topBar = <TopBar title='confirm.topbar' onBack={this.onBack} />
     return (
       <Container topBar={topBar}>
@@ -101,9 +97,6 @@ export default class ConfirmAccount extends Component {
               <Image source={icons.feather} style={styles.image} />
               <Heading2 keyName='confirm.title' style={styles.titleText} />
               <Heading4 keyName='confirm.subtitle' style={styles.subtitleText} />
-            </View>
-
-            <View style={styles.middleFlex}>
               <Heading4 keyName='confirm.label' />
               <Heading3
                 text='{contact}'
@@ -120,12 +113,15 @@ export default class ConfirmAccount extends Component {
                 value={code}
                 onChangeText={(text) => this.onChange(text, 'code')}
               />
-              <Body1
-                keyName='confirm.resendCode'
-                style={styles.resendText}
-                onPress={this.onPressResend}
-              />
-              <Button title='confirm.submit' onPress={this.onPress} />
+              <TouchableOpacity>
+                <Body1
+                  keyName='confirm.resendCode'
+                  style={styles.resendText}
+                  onPress={this.onPressResend}
+                />
+              </TouchableOpacity>
+              <Button title='confirm.submit' onPress={this.onPressContinue} />
+              {signUpVerify ? skipElement : null}
             </View>
           </View>
         </KeyboardAvoidingView>
@@ -141,26 +137,23 @@ const styles = StyleSheet.create({
     height,
     backgroundColor: colors.athensGray,
     paddingHorizontal: width * 0.08,
-    // paddingBottom: height * 0.18,
   },
   topFlex: {
-    flex: 0.3,
+    flex: 0.5,
     alignItems: 'center',
-    paddingTop: height * 0.07,
-    paddingBottom: height * 0.022,
-  },
-  middleFlex: {
-    flex: 0.2,
-    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: height * 0.04,
+    // marginBottom: height * 0.04,
   },
   bottomFlex: {
     flex: 0.5,
     alignItems: 'center',
+    justifyContent: 'flex-start',
   },
   image: {
     width: 79,
     height: 92,
-    marginBottom: height * 0.05,
+    marginBottom: height * 0.03,
   },
   titleText: {
     textAlign: 'center',
@@ -168,10 +161,11 @@ const styles = StyleSheet.create({
   },
   subtitleText: {
     textAlign: 'center',
+    marginBottom: height * 0.04,
   },
   contactText: {
     textAlign: 'center',
-    marginBottom: height * 0.045,
+    marginBottom: height * 0.04,
   },
   noteText: {
     textAlign: 'center',
@@ -180,8 +174,13 @@ const styles = StyleSheet.create({
     alignSelf: 'flex-start',
     color: colors.governorBay,
     textAlign: 'left',
-    marginTop: 5,
-    marginBottom: 35,
-    paddingLeft: 7,
+    marginTop: height * -0.01,
+    marginBottom: height * 0.03,
+    paddingLeft: 8,
+  },
+  skipLabel: {
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+    marginTop: height * 0.04,
   },
 })
