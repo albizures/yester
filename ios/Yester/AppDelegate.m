fa8ff9e28cc52dcf766a84028086fc6c32cd054e
Jose Albizures <albizures3601@gmail.com>
2020-03-28 12:44:45 +0300

    update react-native from  0.61.0 to 0.62.0 part 1

--- ios/Yester/AppDelegate.m
+++ ios/Yester/AppDelegate.m
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 #import "AppDelegate.h"
 
 #import <React/RCTBridge.h>
@@ -13,30 +6,51 @@
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <RNSplashScreen.h>
 #import <React/RCTLinkingManager.h>
-// #import <StoreKit/StoreKit.h>
+
+#if DEBUG
+#import <FlipperKit/FlipperClient.h>
+#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
+#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
+#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
+#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
+#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+
+static void InitializeFlipper(UIApplication *application) {
+  FlipperClient *client = [FlipperClient sharedClient];
+  SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
+  [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
+  [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
+  [client addPlugin:[FlipperKitReactPlugin new]];
+  [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
+  [client start];
+}
+#endif
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+#if DEBUG
+  InitializeFlipper(application);
+#endif
+
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"Yester"
                                             initialProperties:nil];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-  
-  // [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
-  [[FBSDKApplicationDelegate sharedInstance] application:application
-   
-    didFinishLaunchingWithOptions:launchOptions];
 
+  [[FBSDKApplicationDelegate sharedInstance] application:application
+
+    didFinishLaunchingWithOptions:launchOptions];
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  
+
   [RNSplashScreen show];
   return YES;
 }
@@ -70,30 +84,4 @@
   return [RCTLinkingManager application:application openURL:url
                       sourceApplication:sourceApplication annotation:annotation];
 }
-/*
-- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray *)transactions{
-  for(SKPaymentTransaction *transaction in transactions){
-    switch(transaction.transactionState){
-      case SKPaymentTransactionStatePurchasing:
-        [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
-        NSLog(@"Transaction state -> Purchasing");
-        break;
-      case SKPaymentTransactionStatePurchased:
-        [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
-        NSLog(@"Transaction state -> Purchased");
-        break;
-      case SKPaymentTransactionStateRestored:
-        NSLog(@"Transaction state -> Restored");
-        [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
-        break;
-      case SKPaymentTransactionStateFailed:
-        if(transaction.error.code == SKErrorPaymentCancelled){
-          NSLog(@"Transaction state -> Cancelled");
-        }
-        [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
-        break;
-    }
-  }
-}
-*/
 @end
